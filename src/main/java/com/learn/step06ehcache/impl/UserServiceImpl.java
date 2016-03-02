aad30b0859fe41c355b69defd121be0428810990
vince <zhengweizzw@163.com>
2016-03-02 20:43:00 +0800

    step-06 添加EHCache完成

--- src/main/java/com/learn/step06ehcache/impl/UserServiceImpl.java
+++ src/main/java/com/learn/step06ehcache/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.learn.step06ehcache.entity.User;
 import com.learn.step06ehcache.mapper.UserMapper;
 import com.learn.step06ehcache.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,19 @@ public class UserServiceImpl implements UserService {
     @Autowired(required = false)
     private UserMapper userMapper;
 
+
+    /**
+     *
+     * 使用缓存,则不用再访问数据库
+     */
+//    @Cacheable(value = "myCache" ,key="#id + 'findById'")
+    @Cacheable(value = "myCache" )
+    @Override
+    public User findById(Integer id) {
+        System.out.println("@@@@@=>select from db");
+        return this.userMapper.findById(id);
+
+    }
     @Override
     public int insert(User user) {
         return this.userMapper.insert(user);
@@ -50,9 +64,4 @@ public class UserServiceImpl implements UserService {
 
     }
 
-    @Override
-    public User findById(Integer id) {
-        return this.userMapper.findById(id);
-
-    }
 }
